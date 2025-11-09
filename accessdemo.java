072a6d97ea590782e2b0c402f59fbc437dce05cc
nishanthgupta <nishanthgupta156@gmail.com>
2025-11-09 23:47:28 +0530

    Add files via upload

---
+++ accessdemo.java
@@ -0,0 +1,13 @@
+package com.avn.tns.accessdemo;
+
+public class AccessDemo {
+
+	public String name;
+    private int phoneno;
+    protected String id;
+    String branch;
+     public void display(){
+        System.out.println("hello world");
+	}
+
+}
